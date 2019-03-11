fc3282240901da46870270ef08f5e78ff666c363
karine <wukarine@outlook.com>
2019-03-11 16:28:05 +0800

    :wrench: add contentful spaceId and accessToken

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+// require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +30,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID || '',
+        accessToken: process.env.ACCESS_TOKEN || '',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+// const contentful = require('contentful')
+// const client = contentful.createClient({
+//   space: process.env.SPACE_ID,
+//   accessToken: process.env.ACCESS_TOKEN
+// })
+let i = 0
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi karine</h1>
+    <p>{data.contentfulBlogArticalRichTextNode.id}</p>
+    {data.contentfulBlogArticalRichTextNode.content.map(({content}) => (
+      <p key={i++}>{content.value}</p>
+    ))}
+    <p>{data.contentfulBlogArticalRichTextNode.content[0].content[0].value}</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -19,3 +29,18 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    contentfulBlogArticalRichTextNode {
+      id
+      content {
+        nodeType
+        content {
+          value
+          nodeType
+        }
+      }
+    }
+  }
+`
